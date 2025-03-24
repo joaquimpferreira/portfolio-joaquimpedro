70d1eb5a8c2b9d48098e58623cd9022d584df590
joaquimpferreira <joaquimjpfc@gmail.com>
2025-03-24 09:16:52 -0300

    Fix build error: update Vite configuration and remove unnecessary polyfill plugin

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/portfolio-joaquimpedro/",
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
+  },
+  define: {
+    global: 'window'  // Isso pode ser necessário para lidar com objetos globais do navegador
+  },
   build: {
     rollupOptions: {
       output: {
